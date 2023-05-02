3f09f7adf5f6c9a2731b30d72918183513fef93d
vegav <vega.victor.0712@gmail.com>
2023-05-02 10:33:23 +0200

    ajout de connexion et button pour switch de form

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -29,58 +29,99 @@ export const FormLogin = () => {
     setStrength(strengthLabels[strengthIndic]);
   };
   const handleChange = (event) => getStrength(event.target.value);
+  const [showLogin, setShowLogin] = useState(true);
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
 
   return (
-    <div className="login-card">
-      <h2>Inscription</h2>
-      <form className="login-form">
-      <div className="name">
-          <input
-            autoComplete="off"
-            spellCheck="false"
-            className="control"
-            type="text"
-            placeholder="Nom"
-          />
-          <div id="spinner" className="spinner"></div>
+    <>
+      {showLogin ? ( // si showLogin est vrai, affiche la vue de connexion
+        <div className="login-card">
+          <h2>Connexion</h2>
+          <form className="login-form">
+            <div className="username">
+              <input
+                autoComplete="off"
+                spellCheck="false"
+                className="control"
+                type="email"
+                placeholder="Email"
+              />
+              <div id="spinner" className="spinner"></div>
+            </div>
+            <input
+              name="password"
+              spellCheck="false"
+              className="control"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <button className="control" type="button">
+              SE CONNECTER
+            </button>
+            <button onClick={toggleForm}>Basculer vers l'inscription</button>
+          </form>
         </div>
-        <div className="lastName">
-          <input
-            autoComplete="off"
-            spellCheck="false"
-            className="control"
-            type="text"
-            placeholder="Prenom"
-          />
-          <div id="spinner" className="spinner"></div>
+      ) : (
+        // sinon, affiche la vue d'inscription
+        <div className="login-card">
+          <h2>Inscription</h2>
+          <form className="login-form">
+            <div className="name">
+              <input
+                autoComplete="off"
+                spellCheck="false"
+                className="control"
+                type="text"
+                placeholder="Nom"
+              />
+              <div id="spinner" className="spinner"></div>
+            </div>
+            <div className="lastName">
+              <input
+                autoComplete="off"
+                spellCheck="false"
+                className="control"
+                type="text"
+                placeholder="Prenom"
+              />
+              <div id="spinner" className="spinner"></div>
+            </div>
+            <div className="username">
+              <input
+                autoComplete="off"
+                spellCheck="false"
+                className="control"
+                type="email"
+                placeholder="Email"
+              />
+              <div id="spinner" className="spinner"></div>
+            </div>
+            <input
+              name="password"
+              spellCheck="false"
+              className="control"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <div className={`bars ${strength}`}>
+              <div></div>
+            </div>
+            <div className="strength">
+              {strength && <>{strength} password</>}
+            </div>
+            <button className="control" type="button">
+              M'INSCRIRE
+            </button>
+            <button className="goToLogSign" onClick={toggleForm}>Basculer vers la connexion</button>
+          </form>
         </div>
-        <div className="username">
-          <input
-            autoComplete="off"
-            spellCheck="false"
-            className="control"
-            type="email"
-            placeholder="Email"
-          />
-          <div id="spinner" className="spinner"></div>
-        </div>
-        <input
-          name="password"
-          spellCheck="false"
-          className="control"
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        <div className={`bars ${strength}`}>
-          <div></div>
-        </div>
-        <div className="strength">{strength && <>{strength} password</>}</div>
-        <button className="control" type="button">
-          M'INSCRIRE
-        </button>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
