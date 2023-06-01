52163a36ab22585a4b8b369a2238224f7ac33f03
vegav <vega.victor.0712@gmail.com>
2023-06-01 10:12:21 +0200

    Commit de test

--- src/components/LiveTwitch.jsx
+++ src/components/LiveTwitch.jsx
@@ -18,7 +18,7 @@ const LiveTwitch = () => {
           <h3 className="live-title">Diffusions à venir</h3>
           <div className="live-calendar">
             <div className="day-list">
-              <h5>LUNDI 17 AOÛT</h5> <span className="lien-live">Lien de la diffusion</span> 
+              <h5>LUNDI 18 AOÛT</h5> <span className="lien-live">Lien de la diffusion</span> 
             </div>
             <div className="day-list">
               <h5>JEUDI 20 AOÛT</h5> <span className="lien-live">Lien de la diffusion</span> 
