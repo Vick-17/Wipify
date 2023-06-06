33eb236e01b7c2e2caffb25d2d45ba5427e936f2
vegav <vega.victor.0712@gmail.com>
2023-06-06 16:51:49 +0200

    gestion des acce en fonction du role

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,31 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
 import "../styles/page/PageArticle.css";
+import jwt_decode from "jwt-decode";
 
 const Article = () => {
   const [article, setArticle] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+  const token = localStorage.getItem("userToken");
+  const decodedToken = jwt_decode(token);
+  const role = decodedToken.roles;
 
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/article/${id}`);
-        const data = await response.json();
-        setArticle(data);
+        const token = localStorage.getItem("userToken");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+  
+        if (role[0] === "[ROLE_ADMIN]") {
+          const response = await fetch(`http://localhost:8000/article/${id}`, {
+            headers: headers,
+          });
+          const data = await response.json();
+          setArticle(data);
+        } else {
+          navigate("/");
+          console.error("Accès non autorisé");
+        }
       } catch (error) {
         console.error("Error fetching article:", error);
       }
     };
-
+  
     fetchArticle();
-  }, [id]);
+  }, [id, role, navigate]);
+  
+
+
   function formatDate(stringDate) {
     let newDate = new Date(stringDate);
     return newDate.toLocaleDateString("fr");
   }
+
+
   return (
     <>
       <NavBar />
@@ -41,7 +64,10 @@ const Article = () => {
               <h5>{article.title}</h5>
             </div>
           </div>
-          <div className="article-content" dangerouslySetInnerHTML={{ __html: article.content }}></div>
+          <div
+            className="article-content"
+            dangerouslySetInnerHTML={{ __html: article.content }}
+          ></div>
         </div>
       ) : (
         <div>Loading article...</div>
