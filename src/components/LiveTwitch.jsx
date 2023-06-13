7be80c9e868e3bd37c1b0ee8c928d4c00eafacc7
vegav <vega.victor.0712@gmail.com>
2023-06-13 09:45:58 +0200

    Responcive / style du twitchPlayer a l'aide jsx

--- src/components/LiveTwitch.jsx
+++ src/components/LiveTwitch.jsx
@@ -4,21 +4,25 @@ import ButtonAllTwitch from "./ButtonAllTwitch";
 import "../styles/components/LiveTwitch.css";
 
 const LiveTwitch = () => {
+  const twitchPlayer = {
+    with: "50%",
+    height: "480px"
+  }
   return (
     <div>
       <div className="twitch-conatainer">
         <div className="twitch-player">
-          <TwitchPlayer channel="savun" autoplay={false} muted />
+          <TwitchPlayer style={twitchPlayer} channel="savun" autoplay={false} muted />
         </div>
         <div className="live-info">
           <h3 className="live-title">Diffusions à venir</h3>
           <div className="live-calendar">
             <div className="day-list">
-              <h5>LUNDI 18 AOÛT</h5>{" "}
+              <h5>LUNDI 18 AOÛT</h5>
               <span className="lien-live">Lien de la diffusion</span>
             </div>
             <div className="day-list">
-              <h5>JEUDI 20 AOÛT</h5>{" "}
+              <h5>JEUDI 20 AOÛT</h5>
               <span className="lien-live">Lien de la diffusion</span>
             </div>
           </div>
